c55603357c499e0b8ddbfff5086bc900cbcbc9ad
Daniel Chen <team6612@gmail.com>
2017-02-07 20:01:43 +0800

    start mongo connector when updating

--- update_db.sh
+++ update_db.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Starting mongo-connector"
+mongo-connector -m localhost:27017 -t localhost:9200 -d elastic2_doc_manager --admin-username ccns --password CCNSccns &
+MCPID=$!
+echo "Mongo-connector started. PID=$MCPID"
 BASEDIR=$(dirname "$0")
 (cd $BASEDIR/pages && ./update_pages.sh && cd ..)
-node $BASEDIR/run
+node run
+echo "Killing mongo-connector. PID=$MCPID"
+kill $MCPID
+echo "Success"
