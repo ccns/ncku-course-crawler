fa11666f457ca44c456570b4c2dab39cd94a641d
Daniel Chen <team6612@gmail.com>
2017-02-07 21:14:42 +0800

    refactory node code

--- run.js
+++ run.js
@@ -1,36 +1,3 @@
-var sleep = require("sleep");
 var update = require("./update.js");
-var MongoClient = require("mongodb").MongoClient
-var assert = require("assert");
-var config = require("config");
-var url = config.get("mongo.url");
 
-
-// update.updateDeptList();
-// sleep.sleep(3);
-
-function* run() {
-  var db = yield MongoClient.connect(url);
-  var collection = db.collection('depts');
-  var colleges = yield collection.find({}).toArray();
-  db.close();
-
-  for (let i=0; i<colleges.length; i++) {
-    let depts = colleges[i].depts;
-    for (let j=0; j<depts.length; j++) {
-      update.updateCourseList(depts[j]);
-    }
-  }
-}
-
-var gen = run();
-function go(result){
-  if(result.done) return;
-  result.value.then(function(r){
-    go(gen.next(r));
-  }).catch(function(err){
-    console.log(err);
-    return;
-  });
-}
-go(gen.next());
+update(false);
--- update.js
+++ update.js
@@ -3,6 +3,7 @@ var MongoClient = require("mongodb").MongoClient
 var assert = require("assert");
 var config = require("config");
 var url = config.get("mongo.url");
+var sleep = require("sleep");
 
 function updateDeptList() {
   function* run() {
@@ -71,7 +72,37 @@ function updateCourseList(dept) {
   go(gen.next());
 }
 
-module.exports = {
-  updateDeptList: updateDeptList,
-  updateCourseList: updateCourseList
+function update(updateDept) {
+  if(updateDept) {
+    update.updateDeptList();
+    sleep.sleep(3);
+  }
+
+  function* run() {
+    var db = yield MongoClient.connect(url);
+    var collection = db.collection('depts');
+    var colleges = yield collection.find({}).toArray();
+    db.close();
+
+    for (let i=0; i<colleges.length; i++) {
+      let depts = colleges[i].depts;
+      for (let j=0; j<depts.length; j++) {
+        updateCourseList(depts[j]);
+      }
+    }
+  }
+
+  var gen = run();
+  function go(result){
+    if(result.done) return;
+    result.value.then(function(r){
+      go(gen.next(r));
+    }).catch(function(err){
+      console.log(err);
+      return;
+    });
+  }
+  go(gen.next());
 }
+
+module.exports = update
