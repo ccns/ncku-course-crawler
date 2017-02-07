540a4b174af418c0b2faae7171e0953f2d9351fa
Daniel Chen <team6612@gmail.com>
2017-02-07 21:03:33 +0800

    refactory page crawler

--- pages/get_all.py
+++ pages/get_all.py
@@ -2,17 +2,23 @@ import requests
 import hashlib
 import json
 import threading
+import sys
+
+deptFile = "depts.json"
+if(len(sys.argv)>1 and sys.argv[1] == "retry"):
+    deptFile = "retry.json"
 
 requests.adapters.DEFAULT_RETRIES = 10
-dept_f = open("depts.json", "r")
+dept_f = open(deptFile, "r")
 depts = json.load(dept_f)
 dept_f.close()
 
 lock = threading.Lock()
 
 def download(dept):
+    global depts
     url = "http://course-query.acad.ncku.edu.tw/qry/qry001.php?dept_no=" + dept
-    print(url)
+    print("Request sent: "+dept)
 
     req = requests.Session()
     res = req.get(url)
@@ -24,19 +30,15 @@ def download(dept):
     f.write(t.encode("utf8"))
     f.close()
 
-    print(dept+" updated.")
-
     lock.acquire()
     depts.remove(dept)
     f = open("retry.json", "w")
     json.dump(depts, f)
     f.close()
+    print(dept+".html updated. Remain: "+str(len(depts)))
+    if(len(depts)==0):
+        print("All done.")
     lock.release()
 
 for dept in depts:
     threading.Thread(target=download, args=(dept,)).start()
-
-while (len(depts)>0):
-    continue
-
-print("All done.")
--- pages/update_pages.sh
+++ pages/update_pages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 timeout 1m python get_all.py
 while [ "$(<retry.json)" != "[]" ]; do
-  timeout 1m python retry.py
+  echo "Execution timeout, retry ..."
+  timeout 1m python get_all.py retry
 done
