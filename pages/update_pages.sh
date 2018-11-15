58f7927291e6709f9d99a0dc9fd61af4a52c381f
Daniel Chen <team6612@gmail.com>
2018-11-15 19:46:19 +0800

    Update update script

--- pages/update_pages.sh
+++ pages/update_pages.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-timeout 1m python -u get_all.py
+timeout 1m python2 -u get_all.py
+RET=$?
+if [ $RET -gt 0 ] && [ $RET != 124 ]; then
+  exit $RET
+fi
+
 while [ "$(<retry.json)" != "[]" ]; do
   echo "Execution timeout, retry ..."
-  timeout 1m python -u get_all.py retry
+  timeout 1m python2 -u get_all.py retry
+  if [ $RET -gt 0 ] && [ $RET != 124 ]; then
+    exit $RET
+  fi
 done
--- update_db.sh
+++ update_db.sh
@@ -7,8 +7,14 @@ BASEDIR=$(pwd)
 LOGDIR=${1:-$BASEDIR/log}
 LOGFILE=$LOGDIR/update.log
 
+echo -n "Start to fetch all course page ... "
 (time bash -c "cd $BASEDIR/pages && ./update_pages.sh | $BASEDIR/predate.sh >> $LOGFILE && cd ..") 2>&1 | $BASEDIR/predate.sh >> $LOGFILE
+echo "Finish"
+
+echo -n "Start to update course ... "
 node run | $BASEDIR/predate.sh >> $LOGFILE
+echo "Finish"
+
 echo "" | $BASEDIR/predate.sh >> $LOGFILE
 
 TOC=$((`date +%s`-TIC))
